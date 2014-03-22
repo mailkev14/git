a92353135a083f23af642b04bcb08fcdcc8adfb3
Rajesh <rajesh@raastudios.com>
2014-03-23 01:58:38 +0530

    rajesh

--- first.php
+++ first.php
@@ -1,6 +1,8 @@
 <?php
 echo "vinayak:first line";
+echo 'Rajesh: first line below vinayal';
 echo 'Kevin:second line';
 echo "vinayak:second line";
 echo 'Kevin:second round';
+echo 'Rajesh: first line at the end';
 ?>
---
+++ rajesh.php
@@ -0,0 +1,9 @@
+<?php
+	echo 'First Line';
+<<<<<<< HEAD
+	echo 'Second round of changes';
+	echo 'Second Line by me';
+=======
+	echo 'Rajesh has manually added this line';
+>>>>>>> 0a1860447aa1f707b276c4352a1d50cbff2dcdc2
+?>
